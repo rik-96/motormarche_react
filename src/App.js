9dbf0c84922611fb91d7c18a5b7b8fdd82052a0f
rik <dk84864@gmail.com>
2018-07-30 02:30:57 +0530

    Adding services section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MetaTags from 'react-meta-tags';
 import Navigation from './components/Navigation/Navigation';
 import ShopCard from './components/ShopCard/ShopCard';
+import Services from './components/Services/Services';
 import Footer from './components/Footer/Footer';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -13,8 +15,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <MetaTags>
+          <title> MotorMarche </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </MetaTags>
         <Navigation />
         <ShopCard />
+        <Services />
         <Footer />
       </div>
     );
---
+++ src/components/Services/Services.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import './Services.css'
+
+const Services = () => {
+  return (
+    <article>
+  <h2 className="f3 fw4 pa3 mv0">Services</h2>
+  <div className="cf pa2 resp_div">
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+    <a href="/" className="fl resp_img pa4-s pa2-ns rel">
+      <div className="db tc aspect-ratio aspect-ratio--1x1 rel abs">
+        <img src={require("./images/serv6.jpg")} alt="Service Shop1 Album " className="w-100 db outline black-10 aspect-ratio--object "/>
+      </div>
+      <dl className="mt2 f6 lh-copy">
+        <dt className="clip">Name</dt>
+        <dd className="ml0 black truncate w-100">Shop1</dd>
+        <dt className="clip">Service</dt>
+        <dd className="ml0 gray truncate w-100">Service</dd>
+      </dl>
+    </a>
+  </div>
+</article>
+
+  )
+}
+
+export default Services;
